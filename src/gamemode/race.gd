e53a0baa847350065a10e31f107f26558bcc6a73
loiclovitana <loiclovitana@gmail.com>
2024-02-28 12:45:17 +0100

    splitscreen

---
+++ src/gamemode/race.gd
@@ -0,0 +1,12 @@
+extends Node
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
---
+++ src/gamemode/splitted_screen.gd
@@ -0,0 +1,13 @@
+extends SubViewport
+
+
+#@onready var viewport_container : SubViewportContainer = get_parent()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass#if viewport_container.size:
