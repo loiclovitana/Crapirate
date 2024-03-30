3093083285048e9882b38f0a59e85d3a78f0c8e5
loiclovitana <loiclovitana@gmail.com>
2024-03-30 12:21:29 +0100

    bugfix gamepad option

--- src/interface/MainMenu/GamepadOptions.gd
+++ src/interface/MainMenu/GamepadOptions.gd
@@ -11,16 +11,18 @@ func _ready():
 func _process(_delta):
 	var joypads = Input.get_connected_joypads()
 	if len(joypads)+1 != item_count:
-		var previouslySelected = get_selected_id()
+		var previouslySelected = get_selected_metadata()
 		var newly_selected = -1
 		clear()
 		for i in len(joypads):
 			var joypad_id = joypads[i]
-			add_icon_item(GAMEPAD_ICON,str(joypad_id+1)+"  ",joypad_id)
+			add_icon_item(GAMEPAD_ICON,str(joypad_id+1)+"  ")
+			set_item_metadata(i,joypad_id)
 			if joypad_id==previouslySelected:
 				newly_selected=i
 		#add keyboard and select keyboard if no selection
-		add_icon_item(KEYBOARD_ICON,"   ",-1)
+		add_icon_item(KEYBOARD_ICON,"   ")
+		set_item_metadata(-1,-1)
 		if newly_selected==-1:
 			newly_selected=len(joypads)
 		
